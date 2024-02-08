f8a7ba3ebb17f5a375429f308cd433ec2de67989
samuelfsd <samuel.estudo778@gmail.com>
2024-02-08 18:29:22 -0300

    fix: create UserResponseDTO

---
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/dto/User/UserResponseDTO.java
@@ -0,0 +1,54 @@
+package samuelfsd.com.br.myexpenses.dto.User;
+
+import java.util.Date;
+
+public class UserResponseDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+
+    private String profilePhoto;
+
+    private Date dateInactivation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public Date getDateInactivation() {
+        return dateInactivation;
+    }
+
+    public void setDateInactivation(Date dateInactivation) {
+        this.dateInactivation = dateInactivation;
+    }
+}
