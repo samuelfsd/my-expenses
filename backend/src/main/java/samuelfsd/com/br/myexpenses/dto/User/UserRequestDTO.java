67e2b622d51b2761b5a355af4b6cc86a57822f4f
samuelfsd <samuel.estudo778@gmail.com>
2024-02-08 18:21:15 -0300

    fix: add getters and setters to UserDTO

--- backend/src/main/java/samuelfsd/com/br/myexpenses/dto/User/UserRequestDTO.java
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/dto/User/UserRequestDTO.java
@@ -7,4 +7,36 @@ public class UserRequestDTO {
     private String email;
     private String password;
     private String profilePhoto;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
 }
