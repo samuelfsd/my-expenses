5028504473b72a1f9b1ba297d297454d82795a0d
samuelfsd <samuel.estudo778@gmail.com>
2024-03-06 16:41:57 -0300

    fix: change imports in cost center model

--- backend/src/main/java/samuelfsd/com/br/myexpenses/domain/model/CostCenter.java
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/domain/model/CostCenter.java
@@ -1,7 +1,15 @@
 package samuelfsd.com.br.myexpenses.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import java.util.List;
 
