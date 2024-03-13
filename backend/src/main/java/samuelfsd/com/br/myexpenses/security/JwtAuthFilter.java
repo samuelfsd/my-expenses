83cf976c1b03441a1db3c769b94274e601f57c50
samuelfsd <samuel.estudo778@gmail.com>
2024-03-13 20:28:00 -0300

    feat: create attemptAuth

--- backend/src/main/java/samuelfsd/com/br/myexpenses/security/JwtAuthFilter.java
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/security/JwtAuthFilter.java
@@ -1,11 +1,16 @@
 package samuelfsd.com.br.myexpenses.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import samuelfsd.com.br.myexpenses.dto.User.LoginRequestDTO;
 
 public class JwtAuthFilter extends UsernamePasswordAuthenticationFilter {
     private AuthenticationManager authenticationManager;
@@ -21,9 +26,19 @@ public class JwtAuthFilter extends UsernamePasswordAuthenticationFilter {
     }
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-//        try {
-//
-//        } catch (Exception e) {}
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            LoginRequestDTO login = new ObjectMapper().readValue(request.getInputStream(), LoginRequestDTO.class);
+
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(login.getEmail(), login.getPassword());
+
+            Authentication auth = authenticationManager.authenticate(authToken);
+
+            return auth;
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Usuário ou senha inválidos!");
+        } catch (Exception e) {
+            throw new InternalAuthenticationServiceException(e.getMessage());
+        }
     }
 }
