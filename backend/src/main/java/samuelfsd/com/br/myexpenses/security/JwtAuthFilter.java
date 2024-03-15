da398d00f70733378073c77213db81f947fd81b4
samuelfsd <samuel.estudo778@gmail.com>
2024-03-14 21:47:46 -0300

    feat: create succesfulAuth

--- backend/src/main/java/samuelfsd/com/br/myexpenses/security/JwtAuthFilter.java
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/security/JwtAuthFilter.java
@@ -1,21 +1,32 @@
 package samuelfsd.com.br.myexpenses.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import samuelfsd.com.br.myexpenses.domain.model.User;
 import samuelfsd.com.br.myexpenses.dto.User.LoginRequestDTO;
+import samuelfsd.com.br.myexpenses.dto.User.LoginResponseDTO;
+import samuelfsd.com.br.myexpenses.dto.User.UserResponseDTO;
+
+import java.io.IOException;
 
 public class JwtAuthFilter extends UsernamePasswordAuthenticationFilter {
     private AuthenticationManager authenticationManager;
 
     private JwtUtil jwtUtil;
+
+    @Autowired
+    private ModelMapper mapper;
 
     public JwtAuthFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
         super();
@@ -40,5 +51,27 @@ public class JwtAuthFilter extends UsernamePasswordAuthenticationFilter {
         } catch (Exception e) {
             throw new InternalAuthenticationServiceException(e.getMessage());
         }
+    }
+
+
+    @Override
+    protected void successfulAuthentication
+            (HttpServletRequest request,
+             HttpServletResponse response,
+             FilterChain chain,
+             Authentication auth) throws IOException {
+        User user = (User) auth.getPrincipal();
+        String token = jwtUtil.createToken(auth);
+
+        UserResponseDTO userResponseDTO = mapper.map(user, UserResponseDTO.class);
+
+        LoginResponseDTO loginResponseDTO = new LoginResponseDTO();
+        loginResponseDTO.setToken("Bearer" + token);
+        loginResponseDTO.setUser(userResponseDTO);
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+
+        response.getWriter().write(new Gson().toJson(loginResponseDTO));
     }
 }
