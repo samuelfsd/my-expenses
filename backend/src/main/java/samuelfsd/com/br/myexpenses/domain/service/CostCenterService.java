4c64c26c5cf9999135195de05fd762f2c47dbf3d
samuelfsd <samuel.estudo778@gmail.com>
2024-04-03 21:27:07 -0300

    feat: finish cost center service

--- backend/src/main/java/samuelfsd/com/br/myexpenses/domain/service/CostCenterService.java
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/domain/service/CostCenterService.java
@@ -2,13 +2,17 @@ package samuelfsd.com.br.myexpenses.domain.service;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import samuelfsd.com.br.myexpenses.domain.exception.ResourceNotFoundException;
 import samuelfsd.com.br.myexpenses.domain.model.CostCenter;
+import samuelfsd.com.br.myexpenses.domain.model.User;
 import samuelfsd.com.br.myexpenses.domain.repository.CostCenterRepository;
 import samuelfsd.com.br.myexpenses.dto.CostCenter.CostCenterRequestDTO;
 import samuelfsd.com.br.myexpenses.dto.CostCenter.CostCenterResponseDTO;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,21 +35,50 @@ public class CostCenterService implements ICRUDService<CostCenterRequestDTO, Cos
 
     @Override
     public CostCenterResponseDTO getById(Long id) {
-        return null;
+        Optional<CostCenter> costCenter = costCenterRepository.findById(id);
+
+        if (costCenter.isEmpty()) {
+            throw new ResourceNotFoundException("Não foi encontrado um centro de custo com este id " + id);
+        }
+
+        return mapper.map(costCenter.get(), CostCenterResponseDTO.class);
     }
 
     @Override
     public CostCenterResponseDTO create(CostCenterRequestDTO dto) {
-        return null;
+        CostCenter costCenter = mapper.map(dto, CostCenter.class);
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        costCenter.setUser(user);
+        costCenter.setId(null);
+
+        costCenter = costCenterRepository.save(costCenter);
+
+        return mapper.map(costCenter, CostCenterResponseDTO.class);
     }
 
     @Override
     public CostCenterResponseDTO update(Long id, CostCenterRequestDTO dto) {
-        return null;
+        getById(id);
+
+        CostCenter costCenter = mapper.map(dto, CostCenter.class);
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        costCenter.setUser(user);
+        costCenter.setId(id);
+
+        costCenter = costCenterRepository.save(costCenter);
+
+        return mapper.map(costCenter, CostCenterResponseDTO.class);
     }
 
     @Override
     public void delete(Long id) {
+        getById(id);
+
+        costCenterRepository.deleteById(id);
 
     }
 }
