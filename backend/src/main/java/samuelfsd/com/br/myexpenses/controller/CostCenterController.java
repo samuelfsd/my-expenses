c8f766de1518bb232a4f0280e00dc975de4c0441
samuelfsd <samuel.estudo778@gmail.com>
2024-04-03 21:41:51 -0300

    feat: create cost center controller

--- backend/src/main/java/samuelfsd/com/br/myexpenses/controller/CostCenterController.java
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/controller/CostCenterController.java
@@ -13,49 +13,46 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import samuelfsd.com.br.myexpenses.common.ApiPrefix;
-import samuelfsd.com.br.myexpenses.domain.service.UserService;
-import samuelfsd.com.br.myexpenses.dto.User.UserRequestDTO;
-import samuelfsd.com.br.myexpenses.dto.User.UserResponseDTO;
+import samuelfsd.com.br.myexpenses.domain.service.CostCenterService;
+import samuelfsd.com.br.myexpenses.dto.CostCenter.CostCenterRequestDTO;
+import samuelfsd.com.br.myexpenses.dto.CostCenter.CostCenterResponseDTO;
 
 import java.util.List;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping(ApiPrefix.API_V1_PREFIX + "/users")
-public class UserController {
+@RequestMapping(ApiPrefix.API_V1_PREFIX + "/cost-centers")
+public class CostCenterController {
 
     @Autowired
-    private UserService userService;
+    private CostCenterService costCenterService;
 
     @GetMapping
-    public ResponseEntity<List<UserResponseDTO>> getAll(){
-        return ResponseEntity.ok(userService.getAll());
+    public ResponseEntity<List<CostCenterResponseDTO>> listAll() {
+        return ResponseEntity.ok(costCenterService.getAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserResponseDTO> getById(@PathVariable Long id){
-        return ResponseEntity.ok(userService.getById(id));
+    public ResponseEntity<CostCenterResponseDTO> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(costCenterService.getById(id));
     }
 
     @PostMapping
-    public ResponseEntity<UserResponseDTO> create(@RequestBody UserRequestDTO dto) {
-        UserResponseDTO user = userService.create(dto);
-        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    public ResponseEntity<CostCenterResponseDTO> create(@RequestBody CostCenterRequestDTO dto) {
+        CostCenterResponseDTO response = costCenterService.create(dto);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<UserResponseDTO> update(@PathVariable Long id, @RequestBody UserRequestDTO dto){
-        UserResponseDTO user = userService.update(id, dto);
-
-        return ResponseEntity.ok(user);
+    public ResponseEntity<CostCenterResponseDTO> update(@PathVariable Long id, @RequestBody CostCenterRequestDTO dto) {
+        return ResponseEntity.ok(costCenterService.update(id, dto));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
-        userService.delete(id);
+        costCenterService.delete(id);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
-
 }
