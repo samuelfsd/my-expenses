ee6714f4a976229bf0f6541e9d322acedc1fd1cc
samuelfsd <samuel.estudo778@gmail.com>
2024-04-05 21:17:59 -0300

    feat: create Registration model

--- backend/src/main/java/samuelfsd/com/br/myexpenses/domain/model/Registration.java
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/domain/model/Registration.java
@@ -6,7 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import samuelfsd.com.br.myexpenses.domain.Enum.TypeRegistration;
+
+import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Registration {
@@ -15,6 +21,30 @@ public class Registration {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "registration_id")
     private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    private TypeRegistration type;
+
+    @Column(nullable = false)
+    private Double value;
+
+    private Date created_at;
+
+    private Date date_reference;
+
+    private Date expiration_date;
+
+    private Date payment_date;
+
+    @ManyToMany
+    @JoinTable(
+            name = "registration_costcenter",
+            joinColumns = @JoinColumn(name = "registration_id"),
+            inverseJoinColumns = @JoinColumn(name = "costcenter_id")
+    )
+    private List<CostCenter> costCenters;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
