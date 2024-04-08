fd3291147d962d43feacb5731a2ae6c3548567ee
samuelfsd <samuel.estudo778@gmail.com>
2024-04-08 19:59:15 -0300

    feat: add create of RegistrationService

--- backend/src/main/java/samuelfsd/com/br/myexpenses/domain/service/RegistrationService.java
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/domain/service/RegistrationService.java
@@ -2,13 +2,16 @@ package samuelfsd.com.br.myexpenses.domain.service;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import samuelfsd.com.br.myexpenses.domain.exception.ResourceNotFoundException;
 import samuelfsd.com.br.myexpenses.domain.model.Registration;
+import samuelfsd.com.br.myexpenses.domain.model.User;
 import samuelfsd.com.br.myexpenses.domain.repository.RegistrationRepository;
 import samuelfsd.com.br.myexpenses.dto.Registration.RegistrationRequestDTO;
 import samuelfsd.com.br.myexpenses.dto.Registration.RegistrationResponseDTO;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -42,7 +45,17 @@ public class RegistrationService implements ICRUDService<RegistrationRequestDTO,
 
     @Override
     public RegistrationResponseDTO create(RegistrationRequestDTO dto) {
-        return null;
+        Registration registration = mapper.map(dto, Registration.class);
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        registration.setUser(user);
+        registration.setId(null);
+        registration.setCreated_at(new Date());
+
+        registration = registrationRepository.save(registration);
+
+        return mapper.map(registration, RegistrationResponseDTO.class);
     }
 
     @Override
