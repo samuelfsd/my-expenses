24a37e8bd013486f11202d12a4fd6a34925155fa
samuelfsd <samuel.estudo778@gmail.com>
2024-04-10 19:01:03 -0300

    feat: create Query in RegistrationRepository

--- backend/src/main/java/samuelfsd/com/br/myexpenses/domain/repository/RegistrationRepository.java
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/domain/repository/RegistrationRepository.java
@@ -1,6 +1,18 @@
 package samuelfsd.com.br.myexpenses.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import samuelfsd.com.br.myexpenses.domain.model.Registration;
 
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {}
+import java.util.List;
+
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM public.registration" +
+        "WHERE expiration_date " +
+        "BETWEEN TO_TIMESTAMP(:startDate, 'YYYY-MM-DD hh24:MI:SS') AND " +
+        "TO_TIMESTAMP(:endDate, 'YYYY-MM-DD hh24:MI:SS')"
+    )
+    List<Registration> getCashFlowByExpirationDate(@Param("startDate") String startDate, @Param("endDate") String endDate);
+}
