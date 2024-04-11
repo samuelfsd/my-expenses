dd5bd96f17391116839cb5f265fc7adfb8b4e55e
samuelfsd <samuel.estudo778@gmail.com>
2024-04-11 16:47:33 -0300

    feat: create DashboardController

---
+++ backend/src/main/java/samuelfsd/com/br/myexpenses/controller/DashboardController.java
@@ -0,0 +1,27 @@
+package samuelfsd.com.br.myexpenses.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import samuelfsd.com.br.myexpenses.common.ApiPrefix;
+import samuelfsd.com.br.myexpenses.domain.service.DashboardService;
+import samuelfsd.com.br.myexpenses.dto.dashboard.DashboardResponseDTO;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping(ApiPrefix.API_V1_PREFIX + "/dashboard")
+public class DashboardController {
+
+    @Autowired
+    private DashboardService dashboardService;
+
+    public ResponseEntity<DashboardResponseDTO> getCashFlow (
+            @RequestParam(name = "startDate") String startDate,
+            @RequestParam(name = "endDate") String endDate
+    ) {
+        return ResponseEntity.ok(dashboardService.getCashFlow(startDate, endDate));
+    }
+}
