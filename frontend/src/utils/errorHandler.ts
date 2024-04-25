18b2a4441736651248c93138d55e532ffc28945f
samuelfsd <samuel.estudo778@gmail.com>
2024-04-25 19:38:44 -0300

    fix: error handler adjusting

--- frontend/src/utils/errorHandler.ts
+++ frontend/src/utils/errorHandler.ts
@@ -3,18 +3,10 @@ import { toast } from "@/components/ui/use-toast"
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const errorHandler = (error: any) => {
   if (error.response) {
-    if (error.response.data.message.search(':') === -1) {
       toast({
         variant: "destructive",
         title: "Erro",
         description: error.response.data.message
       })
-    }
-    const errorLabel = error.response.data.message.split(':')
-    toast({
-      variant: "destructive",
-      title: "Erro",
-      description: errorLabel
-    })
   }
 }
