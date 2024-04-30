18da3492f5893cb31cb656a637bd4a2adfc708d8
samuelfsd <samuel.estudo778@gmail.com>
2024-04-30 15:02:15 -0300

    fix: change services and api instance

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,21 +1,26 @@
-import axios from 'axios'
+import axios from 'axios';
+import { parseCookies } from 'nookies';
 
-import { useAuthStore } from '@/stores/authStore';
-
-export const instance =  axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
-})
-
-instance.interceptors.request.use(
-  (config) => {
-    const token = useAuthStore.getState().getToken();
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config
+export const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8080/api/v1',
+  headers: {
+    'Content-Type': 'application/json',
   },
-  (error) => {
-    return console.log('erro', error)
+});
+
+// Adicionar o token em cada requisição, se disponível
+axiosInstance.interceptors.request.use((config) => {
+  const cookies = parseCookies({});
+  const token = cookies.token;
+
+  if (token) {
+    config.headers['Authorization'] = token;
   }
-)
+
+  return config;
+}, (error) => {
+  console.log('erro no interceptador: ', error)
+  return Promise.reject(error);
+});
+
+export default axiosInstance;
